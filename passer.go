1bfa8fcb3988b60bce06c1ba8a038dd3bc224bf7
anton <hjjg200@gmail.com>
2019-10-02 04:37:04 +0900

    Create passer.go

---
+++ passer.go
@@ -0,0 +1,30 @@
+package together
+
+import (
+    "time"
+)
+
+type Passer struct {
+	p chan struct{}
+	i time.Duration
+}
+
+func NewPasser(i time.Duration) *Passer {
+	p := make(chan struct{})
+	go func() {
+		p <- struct{}{}
+	}()
+
+	return &Passer{
+		p: p,
+		i: i,
+	}
+}
+
+func (ps *Passer) Pass() {
+	<- ps.p
+	go func() {
+		time.Sleep(ps.i)
+		ps.p <- struct{}{}
+	}()
+}
