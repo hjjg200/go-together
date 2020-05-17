4a281fa1b68f9b1785fa88247824dfb4d5b07fcd
anton <anton>
2020-05-17 02:12:57 +0000

    bench edit

--- poolparty_test.go
+++ poolparty_test.go
@@ -9,7 +9,9 @@ func BenchmarkPoolPartyCPU_0(b *testing.B) {
     pp := NewPoolParty(runtime.NumCPU())
     for i := 0; i < b.N; i++ {
         pp.Join(func() {
-            _ = i
+            for x := 0; x < 100000; x++ {
+                _ = x
+            }
         })
     }
     pp.Close()
@@ -19,7 +21,9 @@ func BenchmarkPoolPartyCPU_1(b *testing.B) {
     pp := NewPoolParty(runtime.NumCPU() + 1)
     for i := 0; i < b.N; i++ {
         pp.Join(func() {
-            _ = i
+            for x := 0; x < 100000; x++ {
+                _ = x
+            }
         })
     }
     pp.Close()
